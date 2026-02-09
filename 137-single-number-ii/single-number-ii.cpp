b91326f2a28ff90748710e12f0d542251d0d86ad
Pritam Biswas <u2104106@student.cuet.ac.bd>
2026-02-09 20:41:34 +0600

    Time: 0 ms (100.00%) | Memory: 14 MB (18.41%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+
+        unordered_map<int,int>freq;
+
+        int one = 0;
+
+        for(int i =0 ; i < nums.size() ; i++){
+          
+          freq[nums[i]]++;
+        }
+        for(auto it : freq){
+            if(it.second == 1){
+                one = it.first;
+                break;
+            }
+        }
+        return one ;
+    }
+};
