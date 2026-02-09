f5a20a38fa7f00d258b4803bbc2bd9030593ce39
Pritam Biswas <u2104106@student.cuet.ac.bd>
2026-02-09 18:28:26 +0600

    Time: 0 ms (100.00%) | Memory: 10.1 MB (36.51%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,31 @@
+//Subset_Bitmanupulation
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+
+        //vector
+        
+        vector<vector<int>>v;
+
+        int n=nums.size();
+
+        for(int i =0 ; i < (1<<n) ; i++){
+
+            vector<int> subset; 
+
+            for(int j = 0 ; j < n ; j++){
+
+                if( i & (1<<j)){
+                    subset.push_back(nums[j]);
+                }
+
+            }
+
+            v.push_back(subset);
+        }
+
+        return v;
+
+    }
+};
