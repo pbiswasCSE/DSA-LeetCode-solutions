9e4348da612a7d755bf57ba948732f8fec4bb58b
Pritam Biswas <u2104106@student.cuet.ac.bd>
2026-02-11 21:09:25 +0600

    Time: 3 ms (24.36%) | Memory: 11.3 MB (19.23%) - LeetSync

---
+++ 338-counting-bits/counting-bits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+
+        vector<int>v;
+
+        for(int i = 0; i <=n ;i++){
+            
+            int x = __builtin_popcount(i);
+            v.push_back(x);
+        }
+        return v;
+    }
+};
